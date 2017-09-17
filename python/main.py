3dc55172f91dcffaa65b9308ec60e51902a97b10
thibault <tbeaumont79@yahoo.com>
2017-09-17 12:53:55 +0200

    new update

--- python/main.py
+++ python/main.py
@@ -1,4 +1,8 @@
-i = 3
-a = 4
-b= a+i
-print(b)
+print "entrez une valeur pour a :"
+a = input()
+print "la valeur de a est ", a
+print "entrez une valeur de b :"
+b = input()
+print "la valeur de b est", b
+if a == 2 & b == 2:
+    print "4"
