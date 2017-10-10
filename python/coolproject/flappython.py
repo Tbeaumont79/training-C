84f420a9c33ea08d6ddb40fe538298e21ef3f079
thibault <tbeaumont79@yahoo.com>
2017-10-10 21:26:50 +0200

    new push

---
+++ python/coolproject/flappython.py
@@ -0,0 +1,12 @@
+import pygame
+import math
+import random
+
+windowY = 9 * 60
+windowX = 16 * 60
+
+init()
+window = display.set_mode((windowX, windowY))
+display.set_caption('flappy bird')
+clock = time.Clock()
+
