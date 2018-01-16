9f50e7e6430c6df1440203af534a3f017cf3cd2c
Beaumont Thibault <bod@Beaumonts-MacBook-Air.local>
2018-01-16 20:54:01 +0100

    new push

---
+++ coursmaison/chainedecaratère/applicationjoinetsplit.py
@@ -0,0 +1,8 @@
+def afficher_flotant(flottant):
+    if type(flottant) is not float:
+        raise TypeError("choisissez un flottant")
+    flottant = str(flottant)
+    partientier, partiflottante = flottant.split(".")
+    a = ",".join ([partientier,partiflottante[:3]])
+    print(a)
+afficher_flotant(3.666677543564567)
---
+++ coursmaison/chainedecaratère/entrainementspli.py
@@ -0,0 +1,2 @@
+test = ["ceci est un test afin de savoir si j 'arrive a m'habituer au clavier qwerty"]
+print(test[1:2])
