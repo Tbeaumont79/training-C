06ecd63d3409e1ff2a20b1b047238e03f7e7e02a
bod <tbeaumont79@yahoo.com>
2018-01-21 22:43:25 +0100

    new push

--- coursmaison/tp_open_classroom/fonction.py
+++ coursmaison/tp_open_classroom/fonction.py
@@ -3,44 +3,60 @@
 from donnees import Words,nombre_de_chance
 import random
 import pickle
+import os
 """fonction"""
-pick = random.choice(Words)
-mot_alea = len(pick) * '*'
-def fonction_replace():
-    global pick
-    global mot_alea
-    global nombre_de_chance
-    print("c'est un mot en {} lettre".format(len(pick)),pick)
-    tab = list(mot_alea)
-    key = 0
-    while pick != mot_alea:
-        if key < nombre_de_chance:
-            print("mot random : ", mot_alea)
-            lettre = input("entrez une lettre : ")
-            if lettre in pick:
-                i = 0
-                for c in pick:
-                    if c == lettre:
-                        tab[i] = lettre
-                    i += 1
-                mot_alea = ''
-                for c in tab:
-                    mot_alea += c
-            else:
-                key+= 1
-                print("il te reste ",nombre_de_chance-key,"chance")
-            if pick == mot_alea:
-                print("nice tu as trouver le mots ")
-                print("vous avez ",nombre_de_chance-key," score")
-            elif key == nombre_de_chance:
-                print("vous n'aviez que ",nombre_de_chance,"chance")
-                quit()
-                        
-                    
-def Creation_score_file():
-    scores = { prenom: nombre_de_chance }
-    with open('score','wb') as score:
-        my_pickler = pickle.Pickler(score)
-        score_total = my_pickler.dump(scores)
-        print(score_total)
 
+
+
+def main():
+
+    while 1:
+        pick = random.choice(Words)
+        mot_alea = len(pick) * '*'
+        prenom = input("enter your name : ")
+        os.system("say 'okay thank you, rules are easy you have 8 trys if you find a letter, you will see by yourself '")
+        global nombre_de_chance
+        print("it's a word in {} letter".format(len(pick)),pick)
+        tab = list(mot_alea)
+        key = 0
+        while pick != mot_alea:
+            if key < nombre_de_chance:
+                print("mot random : ", mot_alea)
+                lettre = input("enter a letter: ")
+                if lettre in pick:
+                    i = 0
+                    for c in pick:
+                        if c == lettre:
+                            tab[i] = lettre
+                        i += 1
+                    mot_alea = ''
+                    for c in tab:
+                        mot_alea += c
+                else:
+                    key+= 1
+                    print("you have  ",nombre_de_chance-key,"try's left")
+                if pick == mot_alea:
+                    os.system("say 'nice you found it !'")
+                    print("your score is  ",nombre_de_chance-key)
+                elif key == nombre_de_chance:
+                    print("you only had ",nombre_de_chance,"try ")
+                    os.system("say 'unfortunatly it's done for you , you are dead ")
+                    quit()
+                            
+                        
+        
+        scores = { prenom: nombre_de_chance-key }
+        with open('score','wb') as score:
+            my_pickler = pickle.Pickler(score)
+            score_total = my_pickler.dump(scores)
+        with open('score','rb') as score:
+            my_depickler = pickle.Unpickler(score)
+            scored = my_depickler.load()
+            print(scored)
+        os.system("say 'Do you want to stop ? write yes , if you want to stop'")
+        stopornot = input("write yes if you want to stop !: ")    
+        if stopornot == "yes" or stopornot == "Yes":
+            quit()
+        else:
+            os.system("say 'ok please write you name ! '")
+            
--- coursmaison/tp_open_classroom/pendu.py
+++ coursmaison/tp_open_classroom/pendu.py
@@ -1,9 +1,14 @@
 """gerer l'inscription du jouer """
 from fonction import * 
 from donnees import *
-prenom = input("entrez votre prenom : ")
-print("trés bien {} bienvenue sur le jeu du pendue --- les régles sont simple tu as 8 chances au départ --- tu dois trouvers les lettres parmis les mots choisi aléatoirement --- si tu arrive à trouver le mot et qu'il te reste 3 chances tu auras 3 points.--- bon courage ---".format(prenom))
-fonction_replace()
-
+import os
+os.system("say 'hello welcome, to bod corporation, here we are making things great for the world, if you want to play the game, please write yes !'")
+anser = input("write yes if you want to continue and no if you want to stop : ")
+if anser == 'yes' or anser == 'Yes':
+    os.system("say 'okay ,first i would like to know your name, Please write your name'")
+    main()
+    
+else:
+    os.system("say 'fine, we hope to see you again, you already miss us'")
 """création d'une fonction créant un fichier Text dans lequel les scores seront enregistrer"""
 """création du fichier score"""    
