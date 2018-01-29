05c75b07ae54d5e24b65d8c7fa901da9e4621833
bod <tbeaumont79@yahoo.com>
2018-01-29 23:06:50 +0100

    new thing

--- coursmaison/les_classes/duree.py
+++ coursmaison/les_classes/duree.py
@@ -10,6 +10,15 @@ class Duree:
     def __str__(self):
         
         return "{0:02} : {1:02}".format(self.min,self.sec)
+    def __add__(self, objet_a_ajouter):
+        nouvelle_duree = Duree()
+        nouvelle_duree.min = self.min
+        nouvelle_duree.sec = self.sec
+
+        nouvelle_duree.sec += objet_a_ajouter
+        if nouvelle_duree.sec >= 60:
+            nouvelle_duree.min += nouvelle_duree.sec // 60
+            nouvelle_duree.sec = nouvelle_duree.sec % 60
+        return nouvelle_duree
 
 test = Duree(3,53)
-print(test)
