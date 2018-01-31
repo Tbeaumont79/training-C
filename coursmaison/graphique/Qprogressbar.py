5c7f6dcce145024b5817809ba70154de3432b429
bod <tbeaumont79@yahoo.com>
2018-02-01 00:05:38 +0100

    get some rest

--- coursmaison/graphique/Qprogressbar.py
+++ coursmaison/graphique/Qprogressbar.py
@@ -1,16 +1,30 @@
-from PySide import QtGui,QtCore
-class FenetrePrincipale(QtGui.QWidget):
-    def __init__(self):
-        super(FenetrePrincipale,self).__init__()
-        self.setWindowTitle('Mon Application')
-        layout = QtGui.QHBoxLayout(self)
-        prg_demo = QtGui.QProgressBar()
-        prg_demo.setRange(0,200)        
-        for i in range(190):
-            i += 1
-            prg_demo.setValue(i)
-        layout.addWidget(prg_demo)
-app = QtGui.QApplication([])
-prg = FenetrePrincipale()
-prg.show()
-app.exec_()
+#!/bin/env python
+
+from PySide import QtGui
+from PySide import QtCore
+import sys
+import time
+
+app = QtGui.QApplication(sys.argv)
+
+window = QtGui.QWidget()
+progress = QtGui.QProgressBar(window)
+progress.setMaximum(100)
+layout = QtGui.QVBoxLayout()
+layout.addWidget(progress)
+window.setLayout(layout)
+
+def update():
+    if progress.value() == progress.maximum():
+        progress.reset()
+        progress.setMaximum(progress.maximum() + 20)
+        progress.setValue(progress.value() + 1)
+
+    timer = QtCore.QTimer(window)
+    timer.start(100)
+    timer.connect(timer, QtCore.SIGNAL('timeout()'), update)
+
+    window.show()
+    time.sleep(8)
+    app.exec_()
+update()
---
+++ coursmaison/graphique/calculatrice.py
@@ -0,0 +1,10 @@
+from PySide import QtCore,QtGui
+class Calculatrice(QtGui.QWidget):
+    def __init__():
+        super(Calculatrice,self).__init__()
+        self.setWindowTitle('Ma super Calculatrice')
+
+
+app = QtGui.QApplication([])
+fenetre = Calculatrice()
+fenetre.show()
