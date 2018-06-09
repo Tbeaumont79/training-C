feac7e5ec4be04487e556ee792c1bdaa42862cee
bod <tbeaumont79@yahoo.com>
2018-06-09 02:22:16 +0200

    new send

--- project/python/coolproject/RessourcesJeux/setup.py
+++ project/python/coolproject/RessourcesJeux/setup.py
@@ -3,7 +3,7 @@ executables = [cx_Freeze.Executable("flappython.py")]
 
 cx_Freeze.setup(
     name = "ballon volant",
-    options ={"build_exe": {"packages":["pygame"],"include_files":["Ballon.png","BradBunR.ttf","NuageBas.png","NuageHaut.png"]}},
+    options ={"build_exe": {"packages":["pygame"],"include_files":["Ballon.png","BradBunR.ttf","NuageBas.png","NuageHaut.png"],"excludes":["tcl","ttk","tkinter","Tkinter"]}},
     executables = executables
 
 )
