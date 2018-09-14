11f50cb6a8d43c1d37aefd5aee224685cf47c05b
bod <tbeaumont79@yahoo.com>
2018-09-14 22:12:07 +0200

    new push bomb generation done

--- demineur/src/ft.h
+++ demineur/src/ft.h
@@ -6,7 +6,7 @@
 /*   By: bod <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/11 18:57:14 by bod               #+#    #+#             */
-/*   Updated: 2018/09/11 21:25:32 by bod              ###   ########.fr       */
+/*   Updated: 2018/09/12 18:50:49 by bod              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
-
+# include <time.h>
 void    ft_putchar(char c);
 void    ft_putstr(char str[size][size]);
 void    display_grid();
--- demineur/src/init_grid.c
+++ demineur/src/init_grid.c
@@ -6,7 +6,7 @@
 /*   By: bod <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/11 17:56:07 by bod               #+#    #+#             */
-/*   Updated: 2018/09/11 22:10:21 by bod              ###   ########.fr       */
+/*   Updated: 2018/09/14 21:34:52 by bod              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,21 +31,40 @@ void    ft_putstr(char str[size][size])
     }
     printf("la valeur de i est %d et la valeur de j est: %d\n",i,j);
 }
-
 void    display_grid()
 {
     int i;
     int j;
+    int count;
+    char motif;
     char grid[size][size]; 
+    int  nbgen;
+    int  nbgen1;
 
     i = 0;
+    count = 0;
+    srand(time(NULL));
+    nbgen = 0;
+    nbgen1 = 0;
     while (size > i)
     {
         j = 0;
         while (size > j) 
         {
+            while (count < 15)//faire un mode hiding pour cacher les bombes.(dépendance de checking)
+            {
+                nbgen = rand()%size;
+                nbgen1 = rand()%size;
+                motif = '#';
+                grid[nbgen][nbgen1] = motif;
+                count++;
+            }
+            if (grid[i][j] != '#')
+            {
+                motif = '.';
+                grid[i][j] = motif;
+            }
             j++;
-            grid[i][j] = '.';
         }
         i++; 
     }
