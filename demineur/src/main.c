a6c7f0d8450db10053f0c0a49785b8ed8e6e1d1d
bod <tbeaumont79@yahoo.com>
2018-09-17 22:14:21 +0200

    new update demineur

--- demineur/src/main.c
+++ demineur/src/main.c
@@ -1,16 +1,19 @@
 
 #include "ft.h"
 
-int     main(int argc, char **argv)
+int     main()
 {
-    if (argc > 1)
+    int x;
+    int y;
+    while (1)
     {
-        //faire un atoi
-        display_grid(ft_atoi(argv[1]),ft_atoi(argv[2]));
-        return (0);
+
+        printf("\n\nplease enter a value for x and y : ");
+        scanf("%d",&x);
+        scanf("%d",&y);
+        printf("x = %d y = %d",x,y);
+        display_grid(x,y);
     }
-    else
-        printf("please enter a value for x and y");
     return (0);
 }
 /* le but est de read les coordonées entrer pas les utilisateurs les valeur son sur une bombe il pert sinon ...
